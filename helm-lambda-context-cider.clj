b0457df49b5bfda30a1c689a57c58fbfcdd454c5
Sean <sean.t.irby@gmail.com>
2016-01-07 23:33:56 -0800

    Sort of works :)

--- helm-lambda-context-cider.clj
+++ helm-lambda-context-cider.clj
@@ -1,9 +1,28 @@
 (ns helm-lambda-context-cider)
 
+(def special-arg-identity "%")
+(def special-arg-nth #"%[0-9]")
+
 (defn wrap-string [s1 s2]
+  "Add s1 to the beginning and end of s2"
   (str s1 s2 s1))
 
+(defn substitute-special-arg [sym single-or-coll]
+  "Substitutes the special args defined by the special-arg-* vars with
+  their proper values. For example, % refers to single-or-coll and %0
+  refers to the 0th element in single-or-coll"
+  (let [sym-str (str sym)]
+    (cond (= sym (symbol special-arg-identity))
+          single-or-coll
+          
+          (not (nil? (re-matches special-arg-nth sym-str)))
+          (single-or-coll (Character/getNumericValue (last sym-str)))
+          
+          :else sym)))
+
 (defmulti make-candidates
+  "Converts data so it can be used as a set of candidates in helm. In
+  other words, a list of the stringed representation of data."
   (fn [data]
     (cond
       (map? data) :default
@@ -23,35 +42,16 @@
   [data]
   (list data))
 
+(defn apply-expression
+  "Given an expression string, expr-str, and a candidate string,
+  c-str, this function will first read each, substitute any special
+  arguments in the expression, and then evaluate the expression."
+  [expr-str c-str]
+  (let [expr (read-string expr-str)
+        c    (read-string c-str)]
+    (eval (map #(substitute-special-arg % c) expr))))
+
 (defn transform-candidates [c-all c-marked expr]
   (if (nil? c-marked)
-    (map #(apply-expression expr %) c-all)
-    (list (apply-expression expr c-marked))))
-
-(defn apply-expression [expr c]
-  (identity expr))
-
-;; (lambda (expression-str x)
-;;          (let ((expression (read expression-str)))
-;;            (if (and (sequencep x) (not (stringp x)))
-;;                (let* ((xs (mapcar 'read x))
-;;                       (ns (number-sequence 0 (1- (length xs))))
-;;                       (arg-names (mapcar (lambda (n) (intern (concat "%" (int-to-string n)))) ns))
-;;                       (% xs)
-;;                       (f `(lambda ,arg-names ,expression)))
-;;                  (apply (eval f) (append xs nil)))
-;;              (let* ((% (read x)))
-;;                (eval expression)))))
-
-
-
-
-
-
-
-
-
-
-
-
-
+    (make-candidates (map  #(apply-expression expr %) c-all))
+    (make-candidates (list (apply-expression expr c-marked)))))
--- helm-lambda-context-cider.el
+++ helm-lambda-context-cider.el
@@ -6,22 +6,16 @@
 (defvar helm-lambda-context-cider-ns "helm-lambda-context-cider")
 (defvar helm-lambda-context-cider-file-name load-file-name)
 
-(defun current-ns ()
-  (nrepl-dict-get (cider-nrepl-sync-request:eval "(ns-name *ns*)") "value"))
 (defun helm-lambda-context-cider-inject ()
   "Makes helm-lambda-context-cider namespace available."
   ;; Remember current ns so it can be switched back to after injection
   (let* ((ns-curr (cider-current-ns)))
-    (my-sp (concat "cider-namespace is: " (cider-current-ns)))
-    (my-sp (concat "current namespace is: " ns-curr))
     (cider-nrepl-sync-request:eval
      (helm-lambda-utils-get-file-string
       (expand-file-name "helm-lambda-context-cider.clj"
                         (file-name-directory
                          (or helm-lambda-context-cider-file-name buffer-file-name)))))
-    (my-sp (concat "current-namespace is: " (current-ns)))
-    (cider-nrepl-sync-request:eval (concat "(in-ns '" ns-curr ")"))
-    (my-sp (concat "current-namespace is: " (current-ns)))))
+    (cider-nrepl-sync-request:eval (concat "(in-ns '" ns-curr ")"))))
 
 (defun helm-lambda-context-cider-require ()
   "Requires helm-lambda-context-cider-ns in current ns fully-qualified."
@@ -33,7 +27,7 @@
 by nrepl.  arg should only be a list, string, or nil."
   (cond ((consp arg) (concat "'" (prin1-to-string arg)))
         ((equal nil arg) "nil")
-        (t (concat "\"" arg "\""))))
+        (t (prin1-to-string arg))))
 
 (defun helm-lambda-context-cider-make-expression-string (fname args stringifyp)
   "Given a clojure function name, fname, and its args, this function
@@ -41,17 +35,15 @@ will create a valid expression string so that it can be passed to
 nrepl for evaluation.  If the args originated from an elispp "
   (let ((expression-list `(
                            "("
-                           ,helm-lambda-context-cider-ns "/"
-                           ,fname
+                           ,helm-lambda-context-cider-ns "/" ,fname
                            ,@(mapcar (lambda (s) (concat " " (if stringifyp (helm-lambda-context-cider-to-arg-string s) s))) args)
                            ")"
                            )))
-    (my-sp (mapconcat 'identity expression-list ""))
     (mapconcat 'identity expression-list "")))
 
 (defun helm-lambda-context-cider-eval (fname args stringifyp)
+  "Used to evaluate one of the functions defined in 'helm-lambda-context-cider.clj'"
   (let ((expression (helm-lambda-context-cider-make-expression-string fname args stringifyp)))
-    (my-sp expression)
     (cider-nrepl-sync-request:eval expression)))
 
 (setq helm-lambda-context-cider
@@ -73,18 +65,16 @@ nrepl for evaluation.  If the args originated from an elispp "
 
        :transform-candidates-try
        (lambda (context candidates-all candidates-marked expression)
-         (setq cider-show-error-buffer t)
+         ;; TODO need to find a way to ignore errors during this time and turn them back on without a buffer popping up
+         (setq cider-show-error-buffer nil)
          (let ((result (helm-lambda-context-cider-eval "transform-candidates"
                                                        `(,candidates-all
                                                          ,candidates-marked
                                                          ,expression)
                                                        t)))
-           (setq cider-show-error-buffer t)
-           (my-sp result)
            (if (equal nil (nrepl-dict-get result "err"))
                (read (nrepl-dict-get result "value"))
-             candidates-all
-             (my-sp result))))
+             candidates-all)))
 
        :transform-candidates
        (lambda (context candidates-all candidates-marked expression)
@@ -95,18 +85,8 @@ nrepl for evaluation.  If the args originated from an elispp "
                                                        t)))
            (read (nrepl-dict-get result "value"))))
 
+       ;; Not needed
        :apply-expression
        (lambda () nil)))
 
-
 (provide 'helm-lambda-context-cider)
-;; Return helm-lambda should eva         (helm-lambda-cider-inject)
-;; ;; Evaluates input and returns message id.
-;; ;; Callback is run multiple times for some reason
-;; (cider-nrepl-request:eval "(+ 1 1)" (lambda (a) (my-sp "running callback") (my-sp a)))
-;; ;;--> delegates to nrepl-request-:eval
-;; ;;  --> delegates to nrepl-send-request
-
-;; (cider-nrepl-sync-request:eval "testyvar" "test.core")
-;; ;;--> delegates to nrepl-sync-request:eval
-;; ;;  --> delegates to nrepl-send-sync-request
