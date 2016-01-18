e3da3ae2b05c3f37e3fc658e214645918e8bc54e
Sean <sean.t.irby@gmail.com>
2016-01-18 12:25:08 -0800

    Add unit test stub

---
+++ test/evalator-context-cider-test.el
@@ -0,0 +1,34 @@
+;;; evalator-context-cider-test.el --- Tests for evalator-context-cider-test.el
+;; 
+;; Author: Sean Irby
+;; Copyright © , Sean Irby
+;; 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; This file is not a part of GNU Emacs
+;; 
+;;; Commentary:
+;; 
+;;; Code:
+
+
+(require 'ert)
+(require 'evalator-context-cider)
+
+(ert-deftest stub-test ())
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; evalator-context-cider-test.el ends here
