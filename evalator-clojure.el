ad9638c5ef990a7ffddf40f46301ab677846667e
Sean <sean.t.irby@gmail.com>
2016-02-06 23:30:29 -0800

    Rename files and update contexts

---
+++ evalator-clojure.el
@@ -0,0 +1,192 @@
+;;; evalator-clojure.el --- Clojure evaluation context for evalator via CIDER.
+;;
+;; Copyright © , Sean Irby
+;; Author: Sean Irby
+;; Maintainer: Sean Irby <sean.t.irby@gmail.com>
+;; URL: http://www.github.com/seanirby/evalator-clojure
+;; Version: 1.0.0
+;; Keywords: languages, clojure, cider, helm
+;; Package-Requires: ((cider "0.10.0") (evalator "1.0.1"))
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; This file is not a part of GNU Emacs
+;;
+;;; Commentary:
+;;
+;;  This package extends the evalator package by adding support for
+;;  the Clojure language.
+;;
+;;  The commands `evalator-clojure' and `evalator-clojure-explicit'
+;;  can each be used to start an evalator session that uses Clojure as
+;;  the evaluation language.
+;;
+;;  `evalator-clojure' will start evalator in normal mode.
+;;
+;;  `evalator-clojure-explicit' will start evalator in explicit mode.
+;;
+;;   NOTE:
+;;   This package uses CIDER to execute Clojure in EMACS so a CIDER
+;;   session must be running and connected to for the functions
+;;   mentioned above to work.
+;;
+;;; Code:
+
+
+(require 'cider)
+(require 'evalator)
+(require 'eieio)
+
+(defvar evalator-clojure-special-arg nil)
+(defvar evalator-clojure-context nil)
+(defvar evalator-clojure-ns "evalator-clojure")
+(defvar evalator-clojure-file-name load-file-name)
+
+;;;###autoload
+(defun evalator-clojure-context ()
+  (if evalator-clojure-context
+      evalator-clojure-context
+    (progn
+      (setq evalator-clojure-context
+            (make-instance
+             'evalator-context
+
+             :name
+             "clojure"
+
+             :special-arg
+             'evalator-clojure-special-arg
+
+             :init
+             'evalator-clojure-init
+
+             :make-candidates
+             'evalator-clojure-make-candidates
+
+             :transform-candidates
+             'evalator-clojure-transform-candidates
+
+             :make-equiv-expr
+             'evalator-clojure-make-equiv-expr)))))
+
+(defun evalator-clojure-inject ()
+  "Make evalator-clojure namespace available."
+  ;; Remember current ns so it can be switched back to after injection
+  (let* ((ns-curr (cider-current-ns)))
+    (cider-nrepl-sync-request:eval
+     (evalator-utils-get-file-string
+      (expand-file-name "evalator-clojure.clj"
+                        (file-name-directory
+                         (or evalator-clojure-file-name buffer-file-name)))))
+    (cider-nrepl-sync-request:eval (concat "(ns " ns-curr ")"))))
+
+(defun evalator-clojure-require ()
+  "Requires evalator-clojure-ns in current ns fully-qualified."
+  (cider-nrepl-sync-request:eval
+   (concat "(require '(" evalator-clojure-ns "))")))
+
+(defun evalator-clojure-swap-special-arg (special-arg-str)
+  "Swaps the special arg defined in evalator-clojure.clj.
+
+Value is swapped with SPECIAL-ARG-STR."
+  (evalator-clojure-eval "swap-special-arg-str" `(,special-arg-str)))
+
+(defun evalator-clojure-to-arg-string (arg)
+  "Convert arg to its string representation.
+
+ARG should only be a list, string, t, or nil."
+  (cond ((consp arg) (concat "'" (prin1-to-string arg)))
+        ((equal t arg) "true")
+        ((equal nil arg) "nil")
+        (t (prin1-to-string arg))))
+
+(defun evalator-clojure-make-expression-string (fname args)
+  "Create an expression string to pass to nrepl.
+
+Accepts a clojure function name, FNAME, and its arguments, ARGS."
+  (let ((expression-list `(
+                           "("
+                           ,evalator-clojure-ns "/" ,fname
+                           ,@(mapcar (lambda (s)
+                                       (concat " " (evalator-clojure-to-arg-string s))) args)
+                           ")"
+                           )))
+    (mapconcat 'identity expression-list "")))
+
+(defun evalator-clojure-eval (fname args)
+  "Evaluate a clojure function.
+
+Accepts a clojure function name, FNAME, and its arguments, ARGS"
+  (let ((expression (evalator-clojure-make-expression-string fname args)))
+    (cider-nrepl-sync-request:eval expression)))
+
+(defun evalator-clojure-result-or-error (result)
+  "Check the result of an nrepl evaluation.
+
+Accepts an nrepl evaluation result, RESULT.  If the result was
+successful read its value and return it.  If an error occured, signal
+an error with the error string contained within RESULT."
+  (let ((val        (nrepl-dict-get result "value"))
+        (error-str  (ansi-color-apply (or (nrepl-dict-get result "ex")
+                                          (nrepl-dict-get result "err")
+                                          "")))
+        (out-str    (ansi-color-apply (or (nrepl-dict-get result "out") ""))))
+    (if val
+        (read val)
+      (signal 'evalator-error (list error-str out-str)))))
+
+(defun evalator-clojure-init ()
+  "See slot documentation in evalator-context.el in evalator package."
+  (if (cider-connections)
+      (progn
+        (evalator-clojure-inject)
+        (evalator-clojure-require)
+        (evalator-clojure-swap-special-arg (evalator-context-get-special-arg evalator-clojure-context)))
+    (progn
+      (message (concat "CIDER must be running before evalator can be started.\n"
+                       "Run 'M-x cider-jack-in' to start a CIDER server and try again."))
+      nil)))
+
+(defun evalator-clojure-make-equiv-expr (exprs)
+  "See slot documentation in evalator-context.el in evalator package."
+  (let ((result (evalator-clojure-eval "make-equiv-expr" `(,exprs))))
+    (read (nrepl-dict-get result "value"))))
+
+(defun evalator-clojure-make-candidates (input mode)
+  "See slot documentation in evalator-context.el in evalator package."
+  (let* ((result (evalator-clojure-eval "make-candidates" `(,input ,mode))))
+    (evalator-clojure-result-or-error result)))
+
+(defun evalator-clojure-transform-candidates (cands expr-str collect-p)
+  "See slot documentation in evalator-context.el in evalator package."
+  (let ((result (evalator-clojure-eval "transform-candidates"
+                                       `(,cands ,expr-str ,collect-p))))
+    (evalator-clojure-result-or-error result)))
+
+;;;###autoload
+(defun evalator-clojure (&optional mode)
+  (interactive)
+  (evalator mode 'evalator-clojure-context))
+
+;;;###autoload
+(defun evalator-clojure-explicit ()
+  (interactive)
+  (evalator-clojure :explicit))
+
+(provide 'evalator-clojure)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; evalator-clojure.el ends here
---
+++ test/evalator-clojure-test.el
@@ -0,0 +1,126 @@
+;;; evalator-clojure-test.el --- Tests for evalator-clojure-test.el
+;;
+;; Author: Sean Irby
+;; Copyright © , Sean Irby
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; This file is not a part of GNU Emacs
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+
+(require 'ert)
+(require 'el-mock)
+(eval-when-compile
+  (require 'cl))
+(require 'evalator-clojure)
+(require 'noflet)
+
+(setq evalator-clojure-special-arg "Ⓔ")
+
+;; Used noflet because I couldn't find a way to mock 'cider-nrepl-sync-request:eval' twice
+(ert-deftest evalator-clojure-inject ()
+  (noflet ((cider-nrepl-sync-request:eval (expr) expr))
+          (with-mock
+           (stub cider-current-ns => "foo.core")
+           (stub evalator-utils-get-file-string => "evalator-clojure.clj contents")
+           (should (equal "(ns foo.core)"
+                          (evalator-clojure-inject))))))
+
+(ert-deftest evalator-clojure-require-test ()
+  (with-mock
+   (stub evalator-clojure-ns => "evalator-clojure")
+   (mock (cider-nrepl-sync-request:eval "(require '(evalator-clojure))") => t)
+   (evalator-clojure-require)))
+
+
+(ert-deftest evalator-clojure-swap-special-arg-test ()
+  (with-mock
+   (mock (evalator-clojure-eval "swap-special-arg-str" '("Ⓔ")) => t)
+   (evalator-clojure-swap-special-arg "Ⓔ")))
+
+(ert-deftest evalator-clojure-to-arg-string-test ()
+  (should (equal "'(\"cand-1\" \"cand-2\" \"cand-3\")"
+                 (evalator-clojure-to-arg-string '("cand-1" "cand-2" "cand-3"))))
+  (should (equal "true"
+                 (evalator-clojure-to-arg-string t)))
+  (should (equal "nil"
+                 (evalator-clojure-to-arg-string nil)))
+  (should (equal ":explicit"
+                 (evalator-clojure-to-arg-string :explicit))))
+
+(ert-deftest evalator-clojure-make-expression-string-test ()
+  (should
+   (equal "(evalator-clojure/my-func '(1 2) \"(+ 1 1)\" nil)"
+          (evalator-clojure-make-expression-string "my-func"
+                                                         '((1 2) "(+ 1 1)" nil)))))
+
+(ert-deftest evalator-clojure-eval-test ()
+  (with-mock
+   (mock (evalator-clojure-make-expression-string "my-func" *) => "(my-func)")
+   (mock (cider-nrepl-sync-request:eval "(my-func)") => t)
+   (evalator-clojure-eval "my-func" nil)))
+
+;; TODO make this more robust once I've finalized error signals
+(ert-deftest evalator-clojure-result-or-error ()
+  (noflet ((nrepl-dict-get (plst prop) (plist-get plst prop)))
+          (with-mock
+           (stub signal => t)
+           (should (equal :foo
+                          (evalator-clojure-result-or-error '("value" ":foo"))))
+           (should (evalator-clojure-result-or-error '("ex" "exception text")))
+           (should (evalator-clojure-result-or-error '("err" "error text")))
+           (should (evalator-clojure-result-or-error '())))))
+
+(ert-deftest evalator-clojure-init ()
+  (with-mock
+   (mock (evalator-context-get-special-arg *) :times 1)
+   (mock (evalator-clojure-inject) :times 1)
+   (mock (evalator-clojure-require) :times 1)
+   (mock (evalator-clojure-swap-special-arg *) :times 1)
+   (let ((cider-mode t))
+     (evalator-clojure-init))
+   (let ((cider-mode nil))
+     (mock (message *))
+     (equal nil
+            (evalator-clojure-init)))))
+
+(ert-deftest evalator-clojure-make-equiv-expr-test ()
+  (with-mock
+   (stub evalator-clojure-eval)
+   (stub nrepl-dict-get => "\"(conj (vector 1 2 3) 4)\"")
+   (should (equal "(conj (vector 1 2 3) 4)"
+                  (evalator-clojure-make-equiv-expr '("(vector 1 2 3)" "(conj Ⓔ 4)"))))))
+
+(ert-deftest evalator-clojure-make-candidates-test ()
+  (with-mock
+   (mock (evalator-clojure-eval "make-candidates" '("(vector 1 2 3)" nil)))
+   (mock (evalator-clojure-result-or-error *) => t)
+   (evalator-clojure-make-candidates "(vector 1 2 3)" nil)))
+
+(ert-deftest evalator-clojure-transform-candidates-test ()
+  (with-mock
+   (mock (evalator-clojure-eval "transform-candidates" '(("foo")
+                                                               "(concat Ⓔ \"bar\")"
+                                                               nil)))
+   (mock (evalator-clojure-result-or-error *) => t)
+   (evalator-clojure-transform-candidates '("foo") "(concat Ⓔ \"bar\")" nil)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; evalator-clojure-test.el ends here
